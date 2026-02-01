3e9e3c98563419e246bc0d092ed31069d565eee4
Nakshatra74 <nakshatra7130@gmail.com>
2026-02-01 19:30:23 +0530

    Simple counter code
    Updated module and signal names for clarity and consistency.

---
+++ Module-1/simple_counter.v
@@ -0,0 +1,13 @@
+module simple_counter (clock, reset, count);
+    input clock, reset;
+    output [31:0] count;
+  reg [31:0] count;  //using reg here means count is a variable that can store a value
+
+  always @(posedge clock or posedge reset)  //at positive edge of clock or reset
+    begin 
+      if (reset)
+        count = 32'b0; // count(32 bit no.) becomes 0 if reset is high, sir has taught the syntax later
+        else
+            count = count + 1;
+    end
+endmodule
