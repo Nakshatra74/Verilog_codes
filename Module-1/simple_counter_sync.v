e9df0ebae1e28778f5a79e69b7fb52e33950b458
Nakshatra74 <nakshatra7130@gmail.com>
2026-02-01 19:45:10 +0530

    synchronous reset in simpler counter
    Removed reset condition from always block.

--- Module-1/simple_counter_sync.v
+++ Module-1/simple_counter_sync.v
@@ -3,7 +3,7 @@ module simple_counter (clock, reset, count);    // counter with synchronous rese
     output [31:0] count;
   reg [31:0] count;  //using reg here means count is a variable that can store a value
 
-  always @(posedge clock or posedge reset)  //at positive edge of clock or reset
+    always @(posedge clock)  //at positive edge of clock or reset
     begin 
       if (reset)
         count = 32'b0; // count(32 bit no.) becomes 0 if reset is high, sir has taught the syntax later
